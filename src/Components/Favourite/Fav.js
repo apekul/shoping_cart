478144f62a7a62feb6a20dcc35c5ca8c274706c9
Adaś <adampekul@gmail.com>
2023-06-20 17:26:24 +0200

    update favourite component content

--- src/Components/Favourite/Fav.js
+++ src/Components/Favourite/Fav.js
@@ -21,7 +21,7 @@ const Fav = () => {
   return (
     <div className="h-full min-h-screen flex flex-col items-center justify-start w-full md:w-full xl:w-2/3 md:m-auto max-w-screen-2xl">
       <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl my-5">
-        Shopping Cart
+        Favourite
       </h1>
       <ul className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 xl:gap-x-8 ">
         {favItems.length > 0 ? (
